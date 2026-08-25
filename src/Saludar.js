a5e2ce5e6bc2b6367dff2992b96b5b4dbf586810
André Ermoth <andres.torrez.ulloa@gmail.com>
2026-08-24 23:16:51 -0400

    Saludar por genero

--- src/Saludar.js
+++ src/Saludar.js
@@ -1,21 +1,31 @@
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 const nombreInput = document.querySelector("#nombre");
-
+const generoInput = document.querySelector("#genero");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombreInput.value;
+  const genero = generoInput.value;
   const hora = new Date().getHours();
   let saludo = "Hola";
+  let saludoGenero = "";
 
+  if (genero === "masculino") {
+    saludoGenero = "Señor";
+  } else if (genero === "femenino") {
+    saludoGenero = "Señora";
+  } else{
+    saludoGenero = "Señor/a";
+  }
   if (hora >= 6 && hora < 12) {
     saludo = "Buenos días";
-  } else if (hora >= 12 && hora < 19) {
+  } else if (hora >= 12 && hora < 18) {
     saludo = "Buenas tardes";
   } else {
     saludo = "Buenas noches";
   }
 
-  div.innerHTML = "<p>" + saludo + ", " + nombre + "!</p>";
+
+  div.innerHTML = "<p>" + saludo + ", " + saludoGenero + " " + nombre + "!</p>";
 });
