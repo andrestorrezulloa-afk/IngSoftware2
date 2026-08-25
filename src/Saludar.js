e6a1d23e4a25339adb86ea1b3993012114a137b7
André Ermoth <andres.torrez.ulloa@gmail.com>
2026-08-24 23:36:41 -0400

    Saludar por edad

--- src/Saludar.js
+++ src/Saludar.js
@@ -3,6 +3,7 @@ const div = document.querySelector("#resultado-div");
 const nombreInput = document.querySelector("#nombre");
 const generoInput = document.querySelector("#genero");
 const idiomaInput = document.querySelector("#idioma");
+const edadInput = document.querySelector("#edad");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,20 +12,29 @@ form.addEventListener("submit", (event) => {
   const genero = generoInput.value.toLowerCase();
   const idioma = idiomaInput.value.toLowerCase();
   const hora = new Date().getHours();
-
+  const edad = parseInt(edadInput.value) || 0;
+  
   let saludo = "";
   let saludoGenero = "";
 
-  if ( idioma === "ingles") {
-  
-    if (genero === "masculino") {
-      saludoGenero = "Mr.";
-    } else if (genero === "femenino") {
-      saludoGenero = "Mrs.";
+  if (idioma === "ingles") {
+    if (edad > 30) {
+      if (genero === "masculino") {
+        saludoGenero = "Mr.";
+      } else if (genero === "femenino") {
+        saludoGenero = "Mrs.";
+      } else {
+        saludoGenero = "Mr./Ms.";
+      }
     } else {
-      saludoGenero = "Mr./Ms.";
+      if (genero === "masculino") {
+        saludoGenero = "Young man";
+      } else if (genero === "femenino") {
+        saludoGenero = "Young woman";
+      } else {
+        saludoGenero = "Young person";
+      }
     }
-
     if (hora >= 6 && hora < 12) {
       saludo = "Good morning";
     } else if (hora >= 12 && hora < 18) {
@@ -33,15 +43,26 @@ form.addEventListener("submit", (event) => {
       saludo = "Good evening";
     }
   } else {
-
-    if (genero === "masculino") {
-      saludoGenero = "Señor";
-    } else if (genero === "femenino") {
-      saludoGenero = "Señora";
+  
+    if (edad > 30) {
+      if (genero === "masculino") {
+        saludoGenero = "Sr.";
+      } else if (genero === "femenino") {
+        saludoGenero = "Sra.";
+      } else {
+        saludoGenero = "Sr./Sra.";
+      }
     } else {
-      saludoGenero = "Señor/a";
+      if (genero === "masculino") {
+        saludoGenero = "Joven";
+      } else if (genero === "femenino") {
+        saludoGenero = "Jovencita";
+      } else {
+        saludoGenero = "Jovencite";
+      }
     }
 
+  
     if (hora >= 6 && hora < 12) {
       saludo = "Buenos días";
     } else if (hora >= 12 && hora < 18) {
