946c1c6a33e80fd76755abcd7c9cfb707321a451
André Ermoth <andres.torrez.ulloa@gmail.com>
2026-08-24 23:08:41 -0400

    Saludar por hora

--- src/Saludar.js
+++ src/Saludar.js
@@ -1,4 +1,3 @@
-
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 const nombreInput = document.querySelector("#nombre");
@@ -7,5 +6,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombreInput.value;
-  div.innerHTML = "<p>" + "Hola, " + nombre + "!" + "</p>";
+  const hora = new Date().getHours();
+  let saludo = "Hola";
+
+  if (hora >= 6 && hora < 12) {
+    saludo = "Buenos días";
+  } else if (hora >= 12 && hora < 19) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  div.innerHTML = "<p>" + saludo + ", " + nombre + "!</p>";
 });
